2488cbc5bc454fce58a6405ee5a3467806965a62
Raman <2020pgicsraman65@poornima.org>
2022-10-29 19:09:50 +0530

    Added Recursion

---
+++ Recursion.java
@@ -0,0 +1,31 @@
+// You can also correct this code by creating a Pull request and I will do much happy by accepting your Pull request
+// sout = System.out.println();
+
+import java.util.Scanner;
+
+class Factorial {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int input = scn.nextInt();
+        System.out.println(factorial(input));
+
+    }
+
+    // factorial method 
+    static int factorial(int n) {
+        if (n == 0) {
+            // This sout statement will tell us that we are in IF condition
+            System.out.println("Inside IF");
+            return 1;
+        } else {
+            // This sout statement will tell us that we are in Else condition
+            System.out.println("Inside Else");
+            return (n * factorial(n - 1));
+        }
+    }
+}
+
+// Time Complexity of this method with Recursion
+// O(n) ("This method is calling himself n times so the time Complexity will be this ")
+// Space Complexity of this method with Recursion
+// O(n) ("This method is calling himself n times so the space Complexity will be this ")
