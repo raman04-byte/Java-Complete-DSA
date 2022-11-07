1d08922edfa5da7061aaaddb191d9129af11b450
Raman <2020pgicsraman65@poornima.org>
2022-11-08 03:44:50 +0530

    Subset of Array

--- Reverse_using_recursion.java
+++ Reverse_using_recursion.java
@@ -1,4 +1,9 @@
+import java.util.Collections;
+import java.util.Vector;
+
 class Solution {
+    public void reverseHelper(int l, int r, Vector<Integer> arr) {
+    }
     public void reverse(){
         
     }
@@ -6,3 +11,12 @@ class Solution {
         
     }
 }
+// void reverseArrayHelper(int l,int r, vector<int>&arr )
+// {
+// 	if(l>=r) return ;
+//     reverseArrayHelper(l+1,r-1,arr);
+//     swap(arr[l],arr[r]);
+// }
+// void reverseArray(vector<int> &arr,int m ){
+//     reverseArrayHelper(m+1,arr.size()-1,arr);
+// }
