90475d6522cc758bbb2c6d983c24101a4a1c421d
Raman Tank <ramantank04022002@gmail.com>
2022-11-13 00:20:52 +0530

    Adding comments in the pattern questions

--- Pattern_Questions/Pattern.java
+++ Pattern_Questions/Pattern.java
@@ -112,10 +112,11 @@ public class Pattern {
             System.out.println();
         }
     }
-    public static void pattern8(int n){
-        for (int row = 1; row <= 2*n; row++) {
-            int c  = row > n ? 2 * n - row : row;
-            for (int space = 0; space < n-c; space++) {
+// Pattern number 8
+    public static void pattern8(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+            int c = row > n ? 2 * n - row : row;
+            for (int space = 0; space < n - c; space++) {
                 System.out.print("  ");
             }
             for (int col = c; col >= 1; col--) {
