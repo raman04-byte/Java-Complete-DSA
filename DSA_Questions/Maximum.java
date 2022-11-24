48922edea796927be7afd5e4978da84c0b764b62
Raman Tank <ramantank04022002@gmail.com>
2022-11-25 01:06:31 +0530

    Array

--- DSA_Questions/Maximum.java
+++ DSA_Questions/Maximum.java
@@ -7,10 +7,20 @@ public class Maximum {
         int arr[] ={1,5,23,6,4};
         swap(arr,1,4);
         System.out.println(Arrays.toString(arr));
+        System.out.println(maximum(arr));
     }
     static void swap(int arr[], int index1, int index2){
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
+    }
+    static int maximum(int[] arr){
+        int maxVal = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
     }
 }
