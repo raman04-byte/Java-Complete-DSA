b74ddd2c0a7ff24495fa8e0266365c731e8714c5
Raman Tank <ramantank04022002@gmail.com>
2022-11-29 03:50:19 +0530

    Added a Missing Number leetcode problem

--- Practise/CyclicSort.java
+++ Practise/CyclicSort.java
@@ -1,10 +1,29 @@
 package Practise;
 
+import java.util.Arrays;
+
 public class CyclicSort {
     public static void main(String[] args) {
-        
+        int[] arr = {3,5,1,2,4};
+        cyclicsort(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    static void cyclicsort(int[] arr){
-        int
+
+    static void cyclicsort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
     }
 }
---
+++ Practise/MissingNumber.java
@@ -0,0 +1,28 @@
+// LeetCode 268
+package Practise;
+
+public class MissingNumber {
+    public int missingNumber(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i];
+            if (arr[i]<arr.length &&arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+        for(int index=0;index<arr.length;index++){
+            if(arr[index]!=index){
+                return index;
+            }
+        }
+        return arr.length;
+    }
+
+    void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
