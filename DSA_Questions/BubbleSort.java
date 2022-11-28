a5700ce5359120d3ea9c2492af571a939e012d3a
Raman Tank <ramantank04022002@gmail.com>
2022-11-29 01:18:43 +0530

    Added Selection sort

--- DSA_Questions/BubbleSort.java
+++ DSA_Questions/BubbleSort.java
@@ -12,16 +12,16 @@ public class BubbleSort {
     public static void bubble(int[] arr) {
         boolean swap;
         for (int i = 0; i < arr.length; i++) {
-            swap=false;
+            swap = false;
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
-                    swap=true;
+                    swap = true;
                 }
             }
-            if(!swap){
+            if (!swap) {
                 break;
             }
         }
