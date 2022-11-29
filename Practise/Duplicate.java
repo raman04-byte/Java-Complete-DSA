d45222873f01817d05d6342205a34f1efb3da590
Raman Tank <ramantank04022002@gmail.com>
2022-11-30 01:33:53 +0530

    Find all the duplicated in the array using cyclic sort

---
+++ Practise/Duplicate.java
@@ -0,0 +1,32 @@
+package Practise;
+
+public class Duplicate {
+    public static void main(String[] args) {
+
+    }
+
+    public int findDuplicate(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] != i + 1) {
+                int correctIndex = arr[i] - 1;
+                if (arr[i] != arr[correctIndex]) {
+                    swap(arr, i, correctIndex);
+                } else {
+                    return arr[i];
+                }
+            } else {
+                i++;
+
+            }
+        }
+
+        return -1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
---
+++ Practise/FindAllDuplicates.java
@@ -0,0 +1,33 @@
+package Practise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllDuplicates {
+
+    public List<Integer> findDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                ans.add(arr[index]);
+            }
+        }
+        return ans;
+    }
+
+    void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
