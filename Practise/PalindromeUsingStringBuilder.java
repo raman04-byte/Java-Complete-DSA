ae53558677a937ff93742e863858747b51e012b4
Raman Tank <ramantank04022002@gmail.com>
2022-12-01 01:56:42 +0530

    Message

--- Practise/PalindromeUsingStringBuilder.java
+++ Practise/PalindromeUsingStringBuilder.java
@@ -7,15 +7,15 @@ public class PalindromeUsingStringBuilder {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter the string");
 
-    String st1 = scn.nextLine();
+        String st1 = scn.nextLine();
 
-    StringBuilder stringBuilder = new StringBuilder(st1);
-    String str2 = String.valueOf(stringBuilder.reverse());
-        if(st1.equals(str2)){
+        StringBuilder stringBuilder = new StringBuilder(st1);
+        String str2 = String.valueOf(stringBuilder.reverse());
+        if (st1.equals(str2)) {
             System.out.println("Palinrome");
-        }else{
+        } else {
             System.out.println("Not Palindrome");
         }
-}
-    
+    }
+
 }
