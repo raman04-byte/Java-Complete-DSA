9edcf8c11a8e1021fcffa85ea394ca2be3ff73a7
Raman Tank <ramantank04022002@gmail.com>
2022-12-01 21:38:22 +0530

    Added Fibonacci series

---
+++ Practise/Fibonacci.java
@@ -0,0 +1,16 @@
+package Practise;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a =6;
+        System.out.println("Ans = "+fibo(a));
+    }
+    static int fibo(int n){
+        if(n<2){
+            return n;
+        }
+        System.out.println(fibo(n-1));
+        System.out.println(fibo(n-2));
+        return fibo(n-1)+fibo(n-2);
+    }
+}
