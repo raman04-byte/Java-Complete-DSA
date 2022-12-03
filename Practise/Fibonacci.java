be407e4a4bee52813e5e567187b11a204453b6b8
Raman Tank <ramantank04022002@gmail.com>
2022-12-04 02:53:32 +0530

    Fibo formula

--- Practise/Fibonacci.java
+++ Practise/Fibonacci.java
@@ -2,15 +2,24 @@ package Practise;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int a =5;
-        System.out.println("Ans = "+fibo(a));
+        int a = 5;
+        
+        // System.out.println("Ans = "+fibo(a));
+        for (int i = 0; i < 11; i++) {
+        System.out.println(fiboFormula(i));    
+        }
     }
-    static int fibo(int n){
-        if(n<2){
+
+    static int fiboFormula(int n){
+        return (int)(Math.pow(((1+Math.sqrt(5))/2),n)/Math.sqrt(5));
+    }
+
+    static int fibo(int n) {
+        if (n < 2) {
             return n;
         }
-        System.out.println(fibo(n-1));
-        System.out.println(fibo(n-2));
-        return fibo(n-1)+fibo(n-2);
+        System.out.println(fibo(n - 1));
+        System.out.println(fibo(n - 2));
+        return fibo(n - 1) + fibo(n - 2);
     }
 }
