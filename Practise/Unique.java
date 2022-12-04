77695a46ee8832608a818cce355a4bf8197e790a
Raman Tank <ramantank04022002@gmail.com>
2022-12-04 23:31:50 +0530

    Formatting

--- Practise/Unique.java
+++ Practise/Unique.java
@@ -2,14 +2,15 @@ package Practise;
 
 public class Unique {
     public static void main(String[] args) {
-        int[] arr={1,1,3,3,4,5,6,6,5};
-System.out.println(ans(arr));
+        int[] arr = { 1, 1, 3, 3, 4, 5, 6, 6, 5 };
+        System.out.println(ans(arr));
 
     }
-    private static int ans(int[] arr){
-        int unique=0;
-        for(int n: arr){
-            unique ^=n;
+
+    private static int ans(int[] arr) {
+        int unique = 0;
+        for (int n : arr) {
+            unique ^= n;
         }
         return unique;
     }
