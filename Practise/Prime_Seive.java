c4445bb64b89c1ceaa7610727f1666992feb68ee
Raman Tank <ramantank04022002@gmail.com>
2022-12-06 22:37:24 +0530

    Prime

--- Practise/Prime_Seive.java
+++ Practise/Prime_Seive.java
@@ -11,12 +11,15 @@ public class Prime_Seive {
         for (int i = 2; i * i <= n; i++) {
             if (!prime[i]) {
                 for (int j = i * 2; j <= n; j += i) {
+                    // Printing the all j value where this loop is iterating 
+                    // System.out.println(j);
                     prime[j] = true;
                 }
             }
         }
         for (int i = 2; i <= n; i++) {
             if (!prime[i]) {
+                // Printing the array of boolean
                 System.out.print(i + " ");
             }
         }
