eb71e869c17b60a6f02560c561d6f139c6662821
Raman Tank <ramantank04022002@gmail.com>
2022-12-06 23:06:46 +0530

    Finding the sqrt of a number using the binary search

---
+++ Practise/Sqrt_UsingBinarySearch.java
@@ -0,0 +1,36 @@
+package Practise;
+
+public class Sqrt_UsingBinarySearch {
+    public static void main(String[] args) {
+        int n=40;
+        int p=3;
+        System.out.println( sqrt(n, p));
+    }
+    public static double sqrt(int n, int p){
+        int s=0;
+        int e=n;
+        double root = 0.0;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            int sqr=m*m;
+            if(sqr==n){
+                return m;
+            }
+            if(sqr>n){
+                e=m-1;
+            }else{
+                s=m+1;
+            }
+
+        }
+        double inc = 0.1;
+        for(int i=0;i<p;i++){
+            while(root*root<=n){
+                root+=inc;
+            }
+            root-=inc;
+            inc/=10;
+        }
+        return root;
+    }
+}
