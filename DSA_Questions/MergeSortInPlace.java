499bc3bcc0e2d74b3e2d6d7750fa3bd95595cc1e
Raman Tank <ramantank04022002@gmail.com>
2022-12-09 01:20:16 +0530

    Merge Sort using the In Place approach

---
+++ DSA_Questions/MergeSortInPlace.java
@@ -0,0 +1,51 @@
+package DSA_Questions;
+
+import java.util.Arrays;
+
+public class MergeSortInPlace {
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1 };
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+        int mid = s + (e - s) / 2;
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+        int i = s;
+        int j = m;
+        int k = 0;
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+        for (int k2 = 0; k2 < mix.length; k2++) {
+            arr[s + k2] = mix[k2];
+        }
+    }
+}
