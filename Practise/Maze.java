6bd1801ff82c2d96103b74aeb6f343edbd474e90
Raman Tank <ramantank04022002@gmail.com>
2022-12-13 02:40:57 +0530

    path of maze

--- Practise/Maze.java
+++ Practise/Maze.java
@@ -3,6 +3,7 @@ package Practise;
 public class Maze {
     public static void main(String[] args) {
         System.out.println(maze(3,3));
+        path("", 3, 3);
     }
     static int maze(int r, int c){
         if(r==1||c==1){
@@ -11,5 +12,14 @@ public class Maze {
         int left=maze(r-1, c);
         int right=maze(r, c-1);
         return left+right;
+    }
+    static void path(String p, int r, int c){
+        if(r==1&&c==1){
+            System.out.println(p);
+        }
+        if(r>1)
+        path(p+'D', r-1, c);
+        if(c>1)
+        path(p+'R', r, c-1);
     }
 }
