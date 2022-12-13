8248c54d19102d39e118e39b0e0224e90e87b706
Raman Tank <ramantank04022002@gmail.com>
2022-12-13 11:26:54 +0530

    Path print

--- Practise/Maze.java
+++ Practise/Maze.java
@@ -1,5 +1,7 @@
 package Practise;
 
+import java.util.Arrays;
+
 public class Maze {
     public static void main(String[] args) {
         // System.out.println(maze(3,3));
@@ -10,7 +12,9 @@ public class Maze {
             { true, true, true }
         };
         // pathRestriction("", arr, 0, 0);
-        allPath("", arr, 0, 0);
+        // allPath("", arr, 0, 0);
+        int[][] path=new int[arr.length][arr[0].length];
+        allPathPrint("", arr, 0, 0,path,1);
     }
 
     static int maze(int r, int c) {
@@ -76,5 +80,35 @@ public class Maze {
         }
         maze[r][c] = true;
 
+    }
+    static void allPathPrint(String p, boolean[][] maze, int r, int c,int[][] path, int step) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c]=step;
+            for(int[]arr:path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+        if (!maze[r][c]) {
+            return;
+        }
+        maze[r][c] = false;
+        path[r][c]=step;
+        if (r < maze.length - 1) {
+            allPathPrint(p + 'D', maze, r + 1, c,path,step+1);
+        }
+        if (c < maze[0].length - 1) {
+            allPathPrint(p + 'R', maze, r, c + 1,path,step+1);
+        }
+        if (r > 0) {
+            allPathPrint(p + 'U', maze, r - 1, c,path,step+1);
+        }
+        if (c > 0) {
+            allPathPrint(p + 'U', maze, r, c - 1,path,step+1);
+        }
+        maze[r][c] = true;
+        path[r][c]=0;
     }
 }
