91216541c292629758de103afa42c7f40b1ece7f
Raman Tank <ramantank04022002@gmail.com>
2022-12-14 12:48:15 +0530

    Fixes in sudoku solver

--- Practise/SudokuSolver.java
+++ Practise/SudokuSolver.java
@@ -2,21 +2,18 @@ package Practise;
 
 public class SudokuSolver {
     public static void main(String[] args) {
-        int[][] board = {
-                { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
-                { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
-                { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
-                { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
-                { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
-                { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
-                { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
-                { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
-        };
-        if(solve(board)){
+        int[][] board = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+        if (solve(board)) {
             display(board);
-        }
-        else{
+        } else {
             System.out.println("Can not solve");
         }
     }
@@ -44,7 +41,7 @@ public class SudokuSolver {
         if (emptyLeft == true) {
             return true;
         }
-        for (int number = 0; number <= 9; number++) {
+        for (int number = 1; number <= 9; number++) {
             if (isSafe(board, row, col, number)) {
                 board[row][col] = number;
                 if (solve(board)) {
@@ -60,8 +57,8 @@ public class SudokuSolver {
 
     private static void display(int[][] board) {
         for (int[] row : board) {
-            for (int col : row) {
-                System.out.print(col + " ");
+            for (int num : row) {
+                System.out.print(num + " ");
             }
             System.out.println();
         }
@@ -69,7 +66,7 @@ public class SudokuSolver {
 
     static boolean isSafe(int[][] board, int row, int column, int num) {
         for (int i = 0; i < board.length; i++) {
-            if (board[row][column] == num) {
+            if (board[row][i] == num) {
                 return false;
             }
         }
@@ -78,12 +75,12 @@ public class SudokuSolver {
                 return false;
             }
         }
-        int sqrt = (int) (Math.sqrt(board.length));
+        int sqrt = (int) Math.sqrt(board.length);
         int start = row - row % sqrt;
         int end = column - column % sqrt;
 
         for (int r = start; r < start + sqrt; r++) {
-            for (int c = end; c < end; c++) {
+            for (int c = end; c < end+sqrt; c++) {
                 if (board[r][c] == num) {
                     return false;
                 }
