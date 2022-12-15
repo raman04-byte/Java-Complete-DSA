3b8dd5c6bbef10247a920e70c7b447edf92204a7
Raman Tank <ramantank04022002@gmail.com>
2022-12-16 01:54:11 +0530

    Commit

--- Practise/RBS.java
+++ Practise/RBS.java
@@ -4,7 +4,7 @@ package Practise;
 public class RBS {
     public static void main(String[] args) {
         int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
-        System.out.println(findPivot(arr));
+        System.out.println(search(arr,5));
     }
 
     static int search(int[] nums, int target) {
