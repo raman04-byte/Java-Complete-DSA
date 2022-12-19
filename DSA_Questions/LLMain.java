2f26c8566f9b1952fa98d842460aa1104f977be1
Raman Tank <ramantank04022002@gmail.com>
2022-12-19 22:30:30 +0530

    Linked list

--- DSA_Questions/LLMain.java
+++ DSA_Questions/LLMain.java
@@ -2,6 +2,12 @@ package DSA_Questions;
 
 public class LLMain {
     public static void main(String[] args) {
-        
+        LL list=new LL();
+
+        list.insertFirst(4);
+        list.insertFirst(3);
+        list.insertFirst(54);
+        list.insertFirst(74);
+        list.display();
     }
 }
