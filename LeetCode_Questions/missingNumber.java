ab30385ff271fdde1a6d9c4ea091809126a1033f
Raman Tank <ramantank04022002@gmail.com>
2022-12-25 16:11:36 +0530

    Missing NUmber

---
+++ LeetCode_Questions/missingNumber.java
@@ -0,0 +1,32 @@
+package LeetCode_Questions;
+// LeetCode question 268
+public class missingNumber {
+    public static void main(String[] args) {
+        
+    }
+    public int missing(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i];
+            if (arr[i]<arr.length &&arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+        for(int index=0;index<arr.length;index++){
+            if(arr[index]!=index){
+                return index;
+            }
+        }
+        return arr.length;
+    }
+
+    void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
+
+}
