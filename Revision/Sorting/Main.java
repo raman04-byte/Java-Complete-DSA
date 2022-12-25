dc144a39f72a52924f9b3d96c88b021d44233b84
Raman Tank <ramantank04022002@gmail.com>
2022-12-25 15:38:51 +0530

    Cyclic sort

--- Revision/Sorting/Main.java
+++ Revision/Sorting/Main.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 //  This will be the main file and I am going to use some OOPS concept
 public class Main {
     public static void main(String[] args) {
-        int arr[] = {1,5,23,6,4};
-        bubbleSort bb = new bubbleSort();
-        bb.bubble(arr);
+        int arr[] = {5,4,3,6,7,8,9,2,1};
+        // bubbleSort bb = new bubbleSort();
+        // bb.bubble(arr);
+        cyclicSort cc=new cyclicSort();
+        cc.cyclicsort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
--- Revision/Sorting/cyclicSort.java
+++ Revision/Sorting/cyclicSort.java
@@ -1,5 +1,22 @@
 package Revision.Sorting;
 
 public class cyclicSort {
-    
+
+    public void cyclicsort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int arr[], int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
