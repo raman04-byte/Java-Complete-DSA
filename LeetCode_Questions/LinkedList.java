ffdd66209cac9d922d480f1871040e6a435237f9
Raman Tank <ramantank04022002@gmail.com>
2022-12-25 16:27:35 +0530

    Removed the error

--- LeetCode_Questions/LinkedList.java
+++ LeetCode_Questions/LinkedList.java
@@ -4,6 +4,22 @@ public class LinkedList {
     public static void main(String[] args) {
         
     }
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
     public ListNode deleteDuplicates(ListNode node) {
         if(node==null){
             return node;
--- LeetCode_Questions/MiddleNode.java
+++ LeetCode_Questions/MiddleNode.java
@@ -4,6 +4,22 @@ public class MiddleNode {
     public static void main(String[] args) {
         
     }
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
     public ListNode middleNode(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
