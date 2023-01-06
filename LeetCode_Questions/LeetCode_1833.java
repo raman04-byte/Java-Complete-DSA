4e246b708d8df5fe355da8654560b1744e0e647e
Raman Tank <ramantank04022002@gmail.com>
2023-01-06 23:39:26 +0530

    Day 36

---
+++ LeetCode_Questions/LeetCode_1833.java
@@ -0,0 +1,22 @@
+package LeetCode_Questions;
+
+import java.util.Arrays;
+
+public class LeetCode_1833 {
+    public static void main(String[] args) {
+        
+    }
+    public int maxIceCream(int[] costs, int coins) {
+        // Store ice cream costs in increasing order.
+        Arrays.sort(costs);
+        int n = costs.length;
+        int answer = 0;
+        // Pick ice creams till we can.
+        while (answer < n && costs[answer] <= coins) {
+            // We can buy this icecream, reduce the cost from the coins. 
+            coins -= costs[answer];
+            answer += 1;
+        }
+        return answer;
+    }
+}
--- Practise/README.txt
+++ Practise/README.txt
@@ -38,3 +38,4 @@ Day 32
 Day 33
 Day 34
 Day 35
+Day 36
