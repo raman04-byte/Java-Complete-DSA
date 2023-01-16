a9148c1e73d51ed07ae52ab8438918a68d685053
Raman Tank <ramantank04022002@gmail.com>
2023-01-16 12:51:38 +0530

    Insertion and deletion in Linked List

--- Revision/LinkedList/Main.java
+++ Revision/LinkedList/Main.java
@@ -3,5 +3,8 @@ package Revision.LinkedList;
 public class Main {
     public static void main(String[] args) {
         LL list= new LL();
+        list.insertFirst(54);
+        list.insertFirst(58);
+        list.display();
     }
 }
