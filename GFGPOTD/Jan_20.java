f6eb7bfa0e0a71b978f7eee93dad0ecdebd052b4
Raman Tank <ramantank04022002@gmail.com>
2023-01-20 21:47:27 +0530

    Geeks for geeks problem of Jan 20

---
+++ GFGPOTD/Jan_20.java
@@ -0,0 +1,33 @@
+package GFGPOTD;
+
+public class Jan_20 {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxWeightCell(int N, int Edge[]) {
+        int[] weights = new int[N];
+        int maxWeight = 0;
+        int maxWeightCell = -1;
+
+        // calculate the weights of all cells
+        for (int i = 0; i < N; i++) {
+            if (Edge[i] != -1) {
+                weights[Edge[i]] += i;
+            }
+        }
+
+        // find the cell with maximum weight
+        for (int i = 0; i < N; i++) {
+            if (weights[i] > maxWeight) {
+                maxWeight = weights[i];
+                maxWeightCell = i;
+            } else if (weights[i] == maxWeight) {
+                maxWeightCell = Math.max(maxWeightCell, i);
+            }
+        }
+
+        return maxWeightCell;
+    }
+
+}
