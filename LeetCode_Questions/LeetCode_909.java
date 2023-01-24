8f5696b71b58db24d91262c2297148d31b7bf97d
Raman Tank <ramantank04022002@gmail.com>
2023-01-24 15:56:26 +0530

    LeetCode 909

---
+++ LeetCode_Questions/LeetCode_909.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+public class LeetCode_909 {
+    public static void main(String[] args) {
+        
+    }
+        public int snakesAndLadders(int[][] board) {
+            int n=board.length;
+            Map<Integer,Integer>hm=new HashMap<>();
+            hm.put(1,0);
+            Queue<Integer>q=new LinkedList<>();
+            q.add(1);//for starting bfs
+            while(!q.isEmpty()){
+                int p=q.poll();
+                if(p==n*n) return hm.get(p);
+                for(int i=p+1;i<=Math.min(p+6,n*n);i++){
+                    int next=check(i,n);
+                    int row=next/n,col=next%n;
+                    int ns=board[row][col]==-1?i:board[row][col];
+                    /*normal BFS*/
+                    if(!hm.containsKey(ns)){
+                        hm.put(ns,hm.get(p)+1);
+                        q.offer(ns);
+                    }
+                }
+            }
+            return -1;
+        }
+        public static int check(int i,int n){
+            int q=(i-1)/n,r=(i-1)%n;
+            int row=n-1-q;
+            int col=row%2!=n%2?r:n-1-r;
+            return row*n+col;
+        }
+    
+}
