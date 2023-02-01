9510870b04babab37161ce585950af64eeea98de
Raman Tank <ramantank04022002@gmail.com>
2023-02-01 20:54:43 +0530

    LeetCode 26

---
+++ LeetCode_Questions/LeetCode_26.java
@@ -0,0 +1,24 @@
+public class LeetCode_26 {
+    public static void main(String[] args) {
+
+    }
+
+    public int removeDuplicates(int[] arr) {
+        int length = arr.length;
+        if (length == 0 || length == 1) {
+            return length;
+        }
+        int[] temp = new int[length];
+        int j = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                temp[j++] = arr[i];
+            }
+        }
+        temp[j++] = arr[length - 1];
+        for (int i = 0; i < j; i++)
+            arr[i] = temp[i];
+
+        return j;
+    }
+}
