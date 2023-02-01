2359383de1b9dd7c44a7917d51127458e4da1ee6
Raman Tank <ramantank04022002@gmail.com>
2023-02-01 20:56:38 +0530

    LeetCode 35

---
+++ LeetCode_Questions/LeetCode_35.java
@@ -0,0 +1,17 @@
+public class LeetCode_35 {
+    public static void main(String[] args) {
+
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            } else if (nums[i] > target) {
+                return i;
+            }
+
+        }
+        return nums.length;
+    }
+}
