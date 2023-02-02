4744fed9baebfdc1298e9b9224cdb000d6ae862e
Raman Tank <ramantank04022002@gmail.com>
2023-02-02 15:37:19 +0530

    LeetCode 57

---
+++ LeetCode_Questions/LeetCode_57.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode_57 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> ans = new ArrayList<>();
+        int[] toAdd = newInterval;
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] > toAdd[1]) {
+                ans.add(toAdd);
+                toAdd = intervals[i];
+            } else if (intervals[i][1] >= toAdd[0])
+                toAdd = new int[] { Math.min(intervals[i][0], toAdd[0]),
+                        Math.max(intervals[i][1], toAdd[1]) };
+            else
+                ans.add(intervals[i]);
+        }
+        ans.add(toAdd);
+        return ans.toArray(new int[ans.size()][2]);
+    }
+
+}
