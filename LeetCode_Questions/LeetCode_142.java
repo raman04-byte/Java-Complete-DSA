f11318abd449c34a046d1aff004e4c295a386046
Raman Tank <ramantank04022002@gmail.com>
2023-02-02 15:40:31 +0530

    LeetCode 142

---
+++ LeetCode_Questions/LeetCode_142.java
@@ -0,0 +1,67 @@
+public class LeetCode_142 {
+    public static void main(String[] args) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        int length = 0;
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = lengthCycle(slow);
+                break;
+            }
+        }
+
+        if (length == 0) {
+            return null;
+        }
+
+        ListNode f = head;
+        ListNode s = head;
+
+        while (length > 0) {
+            s = s.next;
+            length--;
+        }
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+        return s;
+    }
+
+    public int lengthCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                } while (temp != slow);
+                return length;
+            }
+        }
+        return 0;
+    }
+}
