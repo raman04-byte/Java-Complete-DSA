37fae402371589ec09c606e593c063a5ead34365
Raman Tank <ramantank04022002@gmail.com>
2023-02-02 15:39:35 +0530

    LeetCode 162

---
+++ LeetCode_Questions/LeetCode_162.java
@@ -0,0 +1,20 @@
+public class LeetCode_162 {
+    public static void main(String[] args) {
+
+    }
+
+    public int findPeakElement(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                // We are currently is in the decreasing part of an array
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start; // We can return end also cuz the start and the end element are the same
+    }
+}
