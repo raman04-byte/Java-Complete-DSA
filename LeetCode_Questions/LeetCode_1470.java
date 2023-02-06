fc4f219a41bf3977a2a5ddda664c1a31fd91bc82
Raman Tank <ramantank04022002@gmail.com>
2023-02-06 15:52:00 +0530

    LeetCode 1470

---
+++ LeetCode_Questions/LeetCode_1470.java
@@ -0,0 +1,17 @@
+public class LeetCode_1470 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] shuffle(int[] nums, int n) {
+        int count = 0;
+        int[] arr = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            arr[count] = nums[i];
+            arr[count + 1] = nums[i + n];
+            count += 2;
+        }
+        return arr;
+    }
+
+}
