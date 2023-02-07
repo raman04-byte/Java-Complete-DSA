300174fa5b39aec21cc21556705191b6cb32221d
Raman Tank <ramantank04022002@gmail.com>
2023-02-07 20:30:37 +0530

    LeetCode 904

---
+++ LeetCode_Questions/LeetCode_904.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode_904 {
+    public static void main(String[] args) {
+
+    }
+
+    public int totalFruit(int[] tree) {
+        int ans = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int l = 0, r = 0; r < tree.length; ++r) {
+            count.put(tree[r], count.getOrDefault(tree[r], 0) + 1);
+            while (count.size() > 2) {
+                count.put(tree[l], count.get(tree[l]) - 1);
+                count.remove(tree[l], 0);
+                ++l;
+            }
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+}
