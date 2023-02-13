d018f9772aac735f00b9b3b11d7f51dcd3262ea3
Raman Tank <ramantank04022002@gmail.com>
2023-02-13 19:05:38 +0530

    LeetCode 1523

---
+++ LeetCode_Questions/LeetCode_1523.java
@@ -0,0 +1,17 @@
+public class LeetCode_1523 {
+    public static void main(String[] args) {
+
+    }
+
+    public int countOdds(int low, int high) {
+        int total = high - low + 1;
+        if (total % 2 == 0)
+            return total / 2;
+        else {
+            if (low % 2 != 0)
+                return total / 2 + 1;
+            else
+                return total / 2;
+        }
+    }
+}
