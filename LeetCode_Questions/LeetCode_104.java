49243838920affef95a73f5cd3f2539ba4bb499a
Raman Tank <ramantank04022002@gmail.com>
2023-02-16 20:30:07 +0530

    LeetCode 104

---
+++ LeetCode_Questions/LeetCode_104.java
@@ -0,0 +1,33 @@
+public class LeetCode_104 {
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int maxDepth(TreeNode root) {
+        // Base Condition
+        if(root == null) return 0;
+        // Hypothesis
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        // Induction
+        return Math.max(left, right) + 1;
+    }
+}
