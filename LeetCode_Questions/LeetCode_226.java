cd957190743dd948ad879983511188edd385e2d5
Raman Tank <ramantank04022002@gmail.com>
2023-02-18 19:00:57 +0530

    LeetCode 226

---
+++ LeetCode_Questions/LeetCode_226.java
@@ -0,0 +1,35 @@
+public class LeetCode_226 {
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+        return root;
+    }
+}
